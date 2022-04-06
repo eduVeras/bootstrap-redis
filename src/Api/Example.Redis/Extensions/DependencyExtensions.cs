4ca31f1aca790b187daf28e7c5df24841ad06440
Eduardo Veras <edurveras@hotmail.com>
2022-04-05 23:14:03 -0300

    Updated Program, Registering Dependencies

--- src/Api/Example.Redis/Extensions/DependencyExtensions.cs
+++ src/Api/Example.Redis/Extensions/DependencyExtensions.cs
@@ -4,11 +4,8 @@ namespace Example.Redis.Extensions
 {
     public static class DependencyExtensions
     {
-
-        public static IServiceCollection AddDependencies(this IServiceCollection services,IConfiguration config)
+        public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration config)
         {
-
-            //Configure other services up here
             var multiplexer = ConnectionMultiplexer.Connect(config.GetConnectionString("Redis"));
             services.AddSingleton<IConnectionMultiplexer>(multiplexer);
 
--- src/Api/Example.Redis/Program.cs
+++ src/Api/Example.Redis/Program.cs
@@ -1,9 +1,12 @@
+using Example.Redis.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDependencies(builder.Configuration);
 
 var app = builder.Build();
 
